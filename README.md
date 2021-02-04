446aa505034e9ce719419e2f7fa1581729bb418c
murrayhastie <murrayhastie@hotmail.com>
2021-02-04 13:04:08 +0000

    Week 2: + Mongo Integration + Develop badge fix?

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 - Master Build Status: [![Build Status](https://travis-ci.com/murrayhastie/sem.svg?branch=main)](https://travis-ci.com/murrayhastie/sem)
 - Develop Build Status: [![Build Status](https://travis-ci.org/murrayhastie/sem.svg?branch=develop)](https://travis-ci.org/murrayhastie/sem)
-- License: [![LICENSE](https://img.shields.io/github/license/murrayhastie/sem.svg?style=flat-square)](https://github.com/<github-username>/sem/blob/master/LICENSE)
-- Release: [![Releases](https://img.shields.io/github/release/murrayhastie/sem/all.svg?style=flat-square)](https://github.com/<github-username>/sem/releases)
+- License: [![LICENSE](https://img.shields.io/github/license/murrayhastie/sem.svg?style=flat-square)](https://github.com/murrayhastie/sem/blob/master/LICENSE)
+- Release: [![Releases](https://img.shields.io/github/release/murrayhastie/sem/all.svg?style=flat-square)](https://github.com/murrayhastie/sem/releases)
 
 # Murray Hastie 40285583
 # Software Engineering Methods
--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,9 +1,40 @@
 package com.napier.sem;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
+import javax.print.attribute.standard.DocumentName;
+
 public class App
 {
     public static void main(String[] args)
     {
         System.out.println("Initiating Week 2");
+
+        // Connect
+        // Connect to MongoDB on local system - we're using port 27000, start container with -p27000:27017
+        // Connect in a docker container
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
+
+        // Get a database - will create when we use it.
+        MongoDatabase database = mongoClient.getDatabase("mydb");
+
+        // Get a collection from the database
+        MongoCollection<Document> collection = database.getCollection("test");
+
+        // Create a document to store
+        Document doc = new Document("name", "Murray Hastie")
+                .append("class", "Software Engineering Methods")
+                .append("year", "2021")
+                .append("result", new Document("CW", 95).append("EX", 85));
+
+        // Add document to collection
+        collection.insertOne(doc);
+
+        //Check document in collection
+        Document myDoc = collection.find().first();
+        System.out.println(myDoc.toJson());
     }
 }
