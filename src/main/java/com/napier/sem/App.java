e991f249f3fc5314d125a999be9fe8520ff3ef88
murrayhastie <murrayhastie@hotmail.com>
2021-02-07 14:18:34 +0000

    Week 2: Sunday + Server mongo-dbserver

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -14,7 +14,7 @@ public class App
         // Connect
         // Connect to MongoDB on local system - we're using port 27000, start container with -p27000:27017
         // Connect in a docker container
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
 
         // Get a database - will create when we use it.
         MongoDatabase database = mongoClient.getDatabase("mydb");
