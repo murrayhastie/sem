e216c3598995da474c723713dfea5f45c005d0c3
murrayhastie <murrayhastie@hotmail.com>
2021-03-22 19:12:51 +0000

    Lab 05 Complete

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -14,14 +14,13 @@ public class App
         a.connect();
 
         // Extract employee salary information
-        ArrayList<Employee> employees = a.getAllSalaries();
-
-        // Test the size of the returned data - should be 240124
-        System.out.println(employees.size());
+        ArrayList<Employee> employees = a.getAllSalaries_ByRole();
 
         // Print salaries
         a.printSalaries(employees);
 
+        // Test the size of the returned data - should be 240124
+        System.out.println(employees.size());
 
         // Disconnect from database
         a.disconnect();
@@ -140,6 +139,51 @@ public class App
                             + "Manager: " + emp.manager + "\n");
         }
     }
+
+
+    /**
+     *  Gets all the current employees and salaries within a given department.
+     * @return A list of all employees and salaries within a given department, or null if there is an error.
+     */
+    public ArrayList<Employee> getAllSalaries_ByRole()
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+            "SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary "
+                    + "FROM employees, salaries, titles "
+                    + "WHERE employees.emp_no = salaries.emp_no "
+                    + "AND employees.emp_no = titles.emp_no "
+                    + "AND salaries.to_date = '9999-01-01' "
+                    + "AND titles.to_date = '9999-01-01' "
+                    + "AND titles.title = 'Engineer' "
+                    + "ORDER BY employees.emp_no ASC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<Employee> employees = new ArrayList<Employee>();
+            while (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("employees.emp_no");
+                emp.first_name = rset.getString("employees.first_name");
+                emp.last_name = rset.getString("employees.last_name");
+                emp.salary = rset.getInt("salaries.salary");
+                employees.add(emp);
+            }
+            return employees;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get salary details");
+            return null;
+        }
+    }
+
 
     /**
      * Gets all the current employees and salaries.
